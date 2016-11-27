c8ef762b7757df9a0691533ab92574aecd4e2535
menloparkinnovation <menloparkinnovation@gmail.com>
2016-11-27 11:41:53 -0800

    update

--- openpux/deploy/chip/openpux/openpux.sh
+++ openpux/deploy/chip/openpux/openpux.sh
@@ -2,4 +2,4 @@
 DEPLOY=/home/chip/openpux/openpux
 cd $DEPLOY
 
-node openpux.js $DEPLOY/deploy/chip/openpux/chip_awssimpledb_config.json
+node app.js $DEPLOY/deploy/chip/openpux/chip_awssimpledb_config.json
